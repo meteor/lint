2ef06220176aea23cf27e3585ff21c8bf3eea06f
David Greenspan <dgreenspan@alum.mit.edu>
2012-04-26 15:31:26 -0700

    start visualizing AST

--- lint/lint.js
+++ lint/lint.js
@@ -8,9 +8,47 @@ if (Meteor.is_client) {
     if (! source)
       return "";
     var parse = Ugliparse.parse(source);
-    Session.set('parse', parse);
-    return JSON.stringify(parse);
+    return parse;
   };
+
+  Template.show_parse.rest = function(arg) {
+    return (arg || this).slice(1);
+  };
+
+  Template.show_parse.children = function() {
+    switch (this[0]) {
+    case "toplevel":
+      return this[1];
+    case "stat": case "call":
+      return this.slice(1);
+    case "function":
+      return this[3];
+    default:
+      return null;
+    }
+  };
+
+  Template.show_parse.boxhead = function() {
+    switch(this[0]) {
+    case "function":
+      return "function "+(this[1] || "")+"(" +
+        this[2].join(', ') + ")";
+    default:
+      return this[0];
+    }
+  };
+
+  Handlebars.registerHelper('first', function() {
+    return this[0];
+  });
+
+  Handlebars.registerHelper('first_is', function(typ) {
+    return this[0] === typ;
+  });
+
+  Handlebars.registerHelper('stringify', function(arg) {
+    return JSON.stringify(arg);
+  });
 
   Meteor.startup(function() {
     Meteor.call('get_file', 'files/deps.js', function(e, str) {
